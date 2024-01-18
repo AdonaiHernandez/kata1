6a1b6b931b8ce939f085708e70ab4e2cb88def78
Adonai Hernandez Bolaños <adonai.acsii@gmail.com>
2024-01-18 11:18:59 +0000

    Create Rectangle class and methods

---
+++ src/Rectangle.java
@@ -0,0 +1,56 @@
+public class Rectangle {
+
+    private float xPos;
+    private float yPos;
+    private float width;
+    private float height;
+
+    public Rectangle(float xPos, float yPos, float width, float height) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "xPos=" + xPos +
+                ", yPos=" + yPos +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    public float getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(float xPos) {
+        this.xPos = xPos;
+    }
+
+    public float getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(float yPos) {
+        this.yPos = yPos;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
